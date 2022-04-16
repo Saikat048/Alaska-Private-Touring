f00fb3c10a2610aad16d3e021c98e01b9d6250f6
Saikat048 <mdsaikathosan048@gmail.com>
2022-04-17 04:40:17 +0600

    commit login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,30 @@
-// import React from 'react';
-// import { Button, Form } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-// const Login = () => {
-//     return (
-//         <div>
-//             <h1 className='text-primary text-center fw-bold mt-5'>Log In</h1>
-//             <Form className='w-50 mx-auto border p-5'>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Email address</Form.Label>
-//                     <Form.Control type="email" placeholder="Enter email" /> 
-//                 </Form.Group> 
-//                 <Form.Group className="mb-3" controlId="formBasicPassword">
-//                     <Form.Label>Password</Form.Label>
-//                     <Form.Control type="password" placeholder="Password" />
-//                 </Form.Group>  
-//                 <p>Already have an account ?</p> <Link to="/signup">Please Sign Up</Link>
-//                 <Button variant="primary" type="submit">
-//                     Submit
-//                 </Button>
-//             </Form>
-//         </div>
-//     );
-// };
+const Login = () => {
+    return (
+        <div>
+            <h1 className='text-primary text-center fw-bold mt-5'>Log In</h1>
+            <Form className='w-50 mx-auto border p-5'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required/> 
+                </Form.Group> 
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required/>
+                </Form.Group>  
+                <p>Already have an account ? <Link to="/signup">Please Sign Up</Link></p>
 
-// export default Login;
+                <Link to="/">Forgot password?</Link> <br />
+
+                <Button className='mt-4' variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default Login;
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,28 @@
-import React from 'react';
+// import React from 'react';
+// import { Button, Form } from 'react-bootstrap';
 
-const Signup = () => {
-    return (
-        <div>
-            <h2>Sign Up</h2>
-        </div>
-    );
-};
+// const Signup = () => {
+//     return (
+//         <div>
+//             <h2 className='text-primary text-center fw-bold mt-5'>Sign Up</h2>
+//             <Form className='w-50 mx-auto border p-5'>
+//                 <Form.Group className="mb-3" controlId="formBasicEmail">
+//                     <Form.Label>Email address</Form.Label>
+//                     <Form.Control type="email" placeholder="Enter email" required/> 
+//                 </Form.Group>
 
-export default Signup;
+//                 <Form.Group className="mb-3" controlId="formBasicPassword">
+//                     <Form.Label>Password</Form.Label>
+//                     <Form.Control type="password" placeholder="Password" required/>
+//                     <Form.Label>Confirm Password</Form.Label>
+//                     <Form.Control type="password" placeholder="Confirm Password" required/>
+//                 </Form.Group> 
+//                 <Button variant="primary" type="submit">
+//                     Submit
+//                 </Button>
+//             </Form>
+//         </div>
+//     );
+// };
+
+// export default Signup;
